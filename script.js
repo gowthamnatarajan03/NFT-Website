ce78d627339faee97ee7d778fd0fb3032f9d056b
gowthamnatarajan03 <gowthamnatarajan03@gmail.com>
2024-04-23 13:12:17 +0530

    Second Commit, Added a Preloader

--- script.js
+++ script.js
@@ -1,40 +1,59 @@
-////IntroScreen
+////Preloader
+document.addEventListener("DOMContentLoaded", function() {
+  const animationContainer = document.getElementById('preloader');
+  const animation = lottie.loadAnimation({
+      container: animationContainer,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'preloader.json' 
+  });
 
-const intro = document.querySelector('.intro');
-const logo = document.querySelectorAll('.logo');
-const navbar = document.getElementById('navbar');
+  window.addEventListener('load', function() {
+      animation.stop(); 
+      animationContainer.style.display = 'none'; 
+      document.getElementById('content').style.display = 'block'; 
 
-window.addEventListener('DOMContentLoaded', () => {
-  document.body.classList.add('hide-scrollbar');
-  setTimeout(() => {
-    logo.forEach((span, idx) => {
-      setTimeout(() => {
-        span.classList.add('active');
-      }, (idx + 1) * 400);
-    });
-
-    setTimeout(() => {
-      logo.forEach((span, idx) => {
-        setTimeout(() => {
-          span.classList.remove('active');
-          span.classList.add('fade');
-        }, (idx + 1) * 50);
-      });
-    }, 2000);
-
-    setTimeout(() => {
-      intro.style.top = '-100vh';
-      
-    setTimeout(() => {
-      document.body.classList.remove('hide-scrollbar');
-    }, 1000);
-
-    setTimeout(() => {
-      navbar.style.visibility = 'visible';
-    }, 800); 
-    }, 2300);
+      fireIntroScreen();
   });
 });
+
+////IntroScreen
+function fireIntroScreen() {
+  const intro = document.querySelector('.intro');
+  const logo = document.querySelectorAll('.logo');
+  const navbar = document.getElementById('navbar');
+
+  document.body.classList.add('hide-scrollbar');
+  setTimeout(() => {
+      logo.forEach((span, idx) => {
+          setTimeout(() => {
+              span.classList.add('active');
+          }, (idx + 1) * 400);
+      });
+
+      setTimeout(() => {
+          logo.forEach((span, idx) => {
+              setTimeout(() => {
+                  span.classList.remove('active');
+                  span.classList.add('fade');
+              }, (idx + 1) * 50);
+          });
+      }, 2000);
+
+      setTimeout(() => {
+          intro.style.top = '-100vh';
+
+          setTimeout(() => {
+              document.body.classList.remove('hide-scrollbar');
+          }, 1000);
+
+          setTimeout(() => {
+              navbar.style.visibility = 'visible';
+          }, 800);
+      }, 2300);
+  });
+}
 
 ////custom scrollbar
 
